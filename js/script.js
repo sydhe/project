bd8d3c3d68c718457c7af3f7865bcd8a0bdc29be
Sydhe <sydhe@null.net>
2020-10-11 09:55:20 +0300

    JS CYCLES

--- js/script.js
+++ js/script.js
@@ -1,4 +1,25 @@
 "use strict"
 
-const num = 50;
+let num = 50;
+
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
+// }
+
+// do {
+//     console.log(num);
+//     num++;
+// }
+
+// while (num < 55);
+
+
+for (let i = 1; i < 8; i++) {
+    if (i === 6) {
+        // break;
+    continue;
+    }
+    console.log(i);
+}
 
