1cc6028cec5a813511d6b16abe517ac5f892fc6d
Sydhe <sydhe@null.net>
2020-10-14 12:11:11 +0300

    JS Metods

--- js/script.js
+++ js/script.js
@@ -1,36 +1,34 @@
 "use strict";
 
-// let num = 20;
-// function showFirstMessage(text) {
-//     console.log(text);
-//     let num = 10;
-//     console.log(num);
-// }
+// const str = 'test';
+// const arr = [1, 2, 4];
 
-// showFirstMessage('Hello World');
-// console.log(num);
+// // console.log(str[2]);
 
-// function calc(a, b) {
-//     return (a + b);
-// }
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+// console.log(str);
 
-// console.log(calc(4, 3));
-// console.log(calc(5, 6));
-// console.log(calc(10, 6));
+// const fruit = "Some fruit";
 
-// function ret() {
-//     let num = 50;
-//     return num;
-// }
+// console.log(fruit.indexOf('fruit'));
 
-// const anotherNum = ret ();
-// console.log (anotherNum);
+// const logg = "Hello World";
 
-// const logger = function() {
-//     console.log('Hello')
-// };
-// logger();
+// console.log(logg.slice(-5));
 
-const calc = (a, b) => a + b;
-console.log(calc(4, 3));
+// const logg = "Hello World";
 
+// console.log(logg.substring(6, 11));
+
+// const logg = "Hello World";
+
+// console.log(logg.substr(6, 11));
+
+// const num = 12.2;
+// console.log(Math.round(num));
+
+const test = '12.2px';
+// console.log(parseInt(test));
+
+console.log(parseFloat(test));
